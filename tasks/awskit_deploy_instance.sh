5f6f6b166125f65d032a71ecd604eb7882fe6191
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-15 12:45:20 +0100

    bugfix in variable declaration

--- tasks/awskit_deploy_instance.sh
+++ tasks/awskit_deploy_instance.sh
@@ -3,7 +3,7 @@ export AWS_REGION=$PT_aws_region
 export FACTER_aws_region=$PT_aws_region
 export FACTER_user=$PT_aws_user
 puppet apply -e 'include awskit::create_bolt_workshop_master' --detailed-exitcodes
-$retval = $?
+retval=$?
 if [ $retval -eq 0 ] || [ $retval -eq 2 ]; then
   exit 0
 else
