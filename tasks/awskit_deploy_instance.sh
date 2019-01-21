78a700363fe970ad28691359946f7d49295afa62
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-21 13:05:11 +0100

    Update awskit_deploy_instance.sh

--- tasks/awskit_deploy_instance.sh
+++ tasks/awskit_deploy_instance.sh
@@ -2,6 +2,7 @@
 export AWS_REGION=$PT_aws_region
 export FACTER_aws_region=$PT_aws_region
 export FACTER_user=$PT_aws_user
+TASKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 puppet apply -e 'include awskit::create_bolt_workshop_master' --detailed-exitcodes --modulepath "${TASKDIR}/../..:$(puppet config print basemodulepath)"
 retval=$?
 if [ $retval -eq 0 ] || [ $retval -eq 2 ]; then
