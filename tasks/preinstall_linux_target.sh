825d1b5698c58681ebe57faa93bde8c9989d240a
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-03-15 17:04:16 +0100

    Update preinstall_linux_target.sh

--- tasks/preinstall_linux_target.sh
+++ tasks/preinstall_linux_target.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
-sudo yum install -y puppet-bolt nano vim-enhanced.x86_64
+sudo yum install -y puppet-bolt nano vim-enhanced.x86_64 wget
