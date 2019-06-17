6b7d2849725efa6705d2ef0cef21f51808a50fd9
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-06-17 22:50:28 +0200

    Create prep_pe_multi.sh

---
+++ tasks/prep_pe_multi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+yum list installed puppet-agent
+if [ $? -eq 0 ]; then
+  echo 'Puppet Agent from PE is already installed, skipping installation.'
+else
+  cat > ~/pe.conf << FILE
+"pe_install::puppet_master_dnsaltnames": ["master"]
+"puppet_enterprise::puppet_master_host": "%{::trusted.certname}"
+"puppet_enterprise::profile::master::code_manager_auto_configure": true
+"puppet_enterprise::profile::master::r10k_remote": "https://${PT_demoname}-gitlab.classroom.puppet.com/control-repo.git"
+"puppet_enterprise::profile::master::r10k_private_key": "/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa"
+FILE
+
+  sed -i -r -e '/^\s*Defaults\s+secure_path/ s[=(.*)[=\1:/opt/puppetlabs/bin[' /etc/sudoers
+
+  echo '"console_admin_password": "'$PT_admin_pwd'"' >> ~/pe.conf
+
+  mkdir ~/.puppetlabs
+
+  export LANG=en_US.UTF-8
+  export LANGUAGE=en_US.UTF-8
+  export LC_ALL=en_US.UTF-8
+  cd ~/puppet-enterprise*
+  ./puppet-enterprise-installer -p -q -c ~/pe.conf
+
+fi
