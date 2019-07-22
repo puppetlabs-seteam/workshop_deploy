a5919fb64a39da894ca4f38d3b98373f8f1d4f95
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-07-22 21:39:25 +0200

    Account for possible locale issues

--- tasks/generate_token.sh
+++ tasks/generate_token.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 echo ${PT_admin_pwd} | /opt/puppetlabs/bin/puppet-access login --username admin --lifetime 1y
