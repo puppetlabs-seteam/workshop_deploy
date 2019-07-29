9ca2eb9707ff963419a3719311f7a5e790cce03b
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-07-29 19:33:10 +0200

    Create hyra.pp

---
+++ plans/hyra.pp
@@ -0,0 +1,21 @@
+plan workshop_deploy::hydra(
+  String $github_user,
+  String $github_pwd,
+) {
+  run_task(workshop_deploy::check_github_creds, 'localhost', 'Checking Github credentials...', 'username' => $github_user, 'password' => $github_pwd)
+
+  notice('Generating Control Repo student prep scripts...')
+  
+  apply('localhost'){
+    file { '~/prep.ps1':
+      ensure  => file,
+      content => epp('workshop_deploy/prep_ps1.epp', { 'github_user' => $github_user })
+    }
+    file { '~/prep.sh':
+      ensure  => file,
+      content => epp('workshop_deploy/prep_sh.epp', { 'github_user' => $github_user })
+    }
+  }
+
+  run_task(workshop_deploy::setup_control_repo, 'localhost', 'Setting up Control Repo...', 'username' => $github_user, 'password' => $github_pwd)
+}
