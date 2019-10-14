8d303217d9cc198033f016b59448f4e402407ef1
Dimitri Tischenko <dimitri@puppet.com>
2019-10-14 16:04:37 +0200

    Changing autosign setting

--- tasks/configure_autosign.sh
+++ tasks/configure_autosign.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "*.compute.internal" > /etc/puppetlabs/puppet/autosign.conf
-echo "*.classroom.puppet.com" >> /etc/puppetlabs/puppet/autosign.conf
+
+AUTOSIGN_FILE=/etc/puppetlabs/puppet/autosign.conf
+
+echo "*.compute.internal" > $AUTOSIGN_FILE
+echo "*.classroom.puppet.com" >> $AUTOSIGN_FILE
+
+autosign_setting=$(puppet config print autosign --section master)
+
+if [ "${autosign_setting}" != "${AUTOSIGN_FILE}" ] ; then
+  echo "Autosign set to ${autosign_setting}, needs to be $AUTOSIGN_FILE"
+  echo "Setting autosign to $AUTOSIGN_FILE..."
+  puppet config set autosign "$AUTOSIGN_FILE" --section master
+  echo "Restarting puppetserver for new setting to take effect..."
+  systemctl restart pe-puppetserver
+fi
